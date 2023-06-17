b609ff521a7a744aa49b938f466b40748aa8f307
Xander <a.j.drent@st.hanze.nl>
2023-06-17 15:22:19 +0200

    token + aggregated data

--- administrator-angular/administrator-page/src/app/visualisation/visualisation.component.ts
+++ administrator-angular/administrator-page/src/app/visualisation/visualisation.component.ts
@@ -1,15 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-visualisation',
   templateUrl: './visualisation.component.html',
   styleUrls: ['./visualisation.component.css']
 })
-export class VisualisationComponent {
+export class VisualisationComponent implements OnInit {
   isLoggedIn = false;
+  aggregatedData: any;
+  loginMessage: string | undefined;
 
-  constructor() {
-    const jwtToken = localStorage.getItem('jwtToken');
-    this.isLoggedIn = !!jwtToken;
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit() {
+    this.route.queryParamMap.subscribe(params => {
+      const jwtToken = params.get('jwtToken');
+      this.isLoggedIn = !!jwtToken;
+      this.loginMessage = this.isLoggedIn ? 'Welcome, you are logged in.' : 'You are not logged in.';
+
+      if (jwtToken) {
+        this.getAggregatedData(jwtToken);
+      }
+    });
+  }
+
+  getAggregatedData(jwtToken: string) {
+    const apiHeaders = this.createAPIHeaders(jwtToken);
+
+    this.http.get('http://localhost:8000/api/admin/aggregate', { headers: apiHeaders })
+      .subscribe(
+        (response: any) => {
+          this.aggregatedData = response;
+          console.log('Aggregated Data:', this.aggregatedData);
+        },
+        (error: any) => {
+          console.error('Error fetching aggregated data:', error);
+        }
+      );
+  }
+
+  createAPIHeaders(jwtToken: string | null): { 'Content-Type': string; Authorization?: string } {
+    const headers: { 'Content-Type': string; Authorization?: string } = {
+      'Content-Type': 'application/json'
+    };
+
+    if (jwtToken) {
+      headers['Authorization'] = `Bearer ${jwtToken}`;
+    }
+
+    return headers;
   }
 }
